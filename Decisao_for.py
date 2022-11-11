d7517fe8457b81a8837778534b2a64740b1b5687
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-11-11 19:00:44 -0300

    Delete Decisao_for.py

--- Decisao_for.py
+++
@@ -1,7 +0,0 @@
-import msvcrt
-
-tabuada=int(input("Digite o Numero para exibir a tabuada: "))
-print("Tabuada do Numero "+ str(tabuada))
-for valor in range(0,11,1):
-    print(str(tabuada)+"x"+ str(valor)+"="+str(tabuada*valor))
-msvcrt.getch()
