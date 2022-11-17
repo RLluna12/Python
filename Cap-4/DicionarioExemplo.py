2f7fa643ac7319172940f708424cbc2672e93be4
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-11-17 17:32:18 -0300

    Add files via upload

---
+++ Cap-4/DicionarioExemplo.py
@@ -0,0 +1,13 @@
+usuarios={}
+print(usuarios)
+
+usuarios ={
+    "chaves" : ["chaves do 8","24/12/2017","Recep_01"],
+    "Quico":["Quico das flores","20/12/2017", "Raiox_03"]
+}
+print(usuarios)
+
+usuarios["Florinda"]=["Dona Florinda", "24/12/2017","Raiox_01"]
+print(usuarios)
+print("####-------------#####")
+print("Dados: ",usuarios.get("Quico"))
